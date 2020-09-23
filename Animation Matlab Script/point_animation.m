0d9495b02e9181b381dd4ffe4164acd109736889
Abhinav Roy <abhinavroy1999@gmail.com>
2020-09-24 00:49:04 +0530

    Update point_animation.m

--- Animation Matlab Script/point_animation.m
+++ Animation Matlab Script/point_animation.m
@@ -1,5 +1,7 @@
-% creating a script got a point animation
-theta = linspace(0, 1, 500);   % creating the independent variable
+% Creating a script for a point animation.
+% Author: Abhinav Roy
+
+theta = linspace(0, 1, 500);    % creating the independent variable
 x = sin(2*pi*theta);            % creating the y-data
 y = cos(2*pi*theta);            % creating the x-data 
 
@@ -10,17 +12,17 @@ title('time 1');
 xlim([-2,2]);
 ylim([-1.5,1.5]);
 
-% creating and writing a video writer
-vw = VideoWriter('point_animation.avi');
-vw.Quality = 100;
-vw.FrameRate = 60;
+% creating and writing a Video Writer
+vw = VideoWriter('point_animation.avi');    % using the VideoWriter function.
+vw.Quality = 100;                           % setting the quality of the video file. 
+vw.FrameRate = 60;                          % setting the frame rate of the video.
 open(vw);
 
 % animation Loop
 for i = 1:length(x)
-    set(h,'XData',x(i),'YData',y(i));
-    title(sprintf("time %d",i));
-    drawnow;
+    set(h,'XData',x(i),'YData',y(i));       % looping the successive plots by setting the 
+    title(sprintf("time %d",i));            % handle to the plot file.
+    drawnow;                                % to render the animation in the figure window.
     frame = getframe(gcf);
     writeVideo(vw, frame);
 end  
